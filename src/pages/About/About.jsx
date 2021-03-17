c96b2ad871a5cdbef6737c723c75157e724e642e
Patti Perlock <pattiperlock@gmail.com>
2021-03-17 06:48:11 -0400

    Set nav links to set width, fixed arrow animation

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -6,6 +6,7 @@ function About (){
         <>
             <h2 className="about__tagline"> Builder, creator, problem solver, and life long learner adventuring through the Web Development landscape. </h2>
             <p className="about__journey"><a href="#skills" className="about__journey-link">Follow me on my journey</a></p>
+            {/* <p className="about__journey"><p className="about__journey-link">Follow me on my journey</p></p> */}
         </>
     )
 }
