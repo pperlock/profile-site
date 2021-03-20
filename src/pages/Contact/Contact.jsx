78c7433162f4540535993952d15235e05abd0a00
brainstation-oll <65925093+brainstation-oll@users.noreply.github.com>
2021-03-20 11:04:17 -0400

    Removed heights based on vh - fixing responsive issues

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,6 +7,13 @@ function Contact() {
             <h2 className="contact__content-quote"> “Becoming isn’t about arriving somewhere or achieving a certain aim. I see it instead as forward motion, a means of evolving, a way to reach continuously toward a better self. The journey doesn’t end.”</h2>
             <p className="contact__content-author">~ Michelle Obama </p>
             <p className="contact__content-cta"> Continue the Adventure </p>
+
+            <div className = "main__contacts-list">
+                <a className="main__contacts-list-text" href="https://github.com/pperlock" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="900" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">github.com/pperlock</a>
+                <a className="main__contacts-list-text" href="https://www.linkedin.com/in/pattiperlock" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="950" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">linkedin.com/in/pattiperlock</a>
+                <a className="main__contacts-list-text" href="mailto:pattiperlock@gmail.com" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="1000" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">pattiperlock@gmail.com</a>
+                <a className="main__contacts-list-text" href="/files/patti-perlock-resume-revised.pdf" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="1000" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">Resume</a>
+            </div>
         </> 
     )
 }
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -47,16 +47,6 @@ function MainPage({match}) {
 
                 {/* renders a Section component for each individual section */}
                 {sections.map((section,i) => <Section key={i} section={section} reachedBottom={reachedBottom}/>)}
-
-                {/* renders the text associated with the contact icons if the bottom of the page is reached */}
-                {reachedBottom && 
-                    <div className = "main__contacts-list">
-                        <a className="main__contacts-list-text" href="https://github.com/pperlock" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="900" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">github.com/pperlock</a>
-                        <a className="main__contacts-list-text" href="https://www.linkedin.com/in/pattiperlock" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="950" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">linkedin.com/in/pattiperlock</a>
-                        <a className="main__contacts-list-text" href="mailto:pattiperlock@gmail.com" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="1000" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">pattiperlock@gmail.com</a>
-                        <a className="main__contacts-list-text" href="/files/patti-perlock-resume-revised.pdf" target="_blank" rel="noopener noreferrer" data-aos-anchor=".contact" data-aos-delay="1000" data-aos="fade-left" data-aos-offset="500" data-aos-duration="500">Resume</a>
-                    </div>
-                }
             </main>
 
         </>
