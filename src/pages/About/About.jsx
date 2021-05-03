857da30bbdc19d2f6e3da85a96fbff0d68d101c1
Patti Perlock <pattiperlock@gmail.com>
2021-05-03 08:58:35 -0400

    Preloaded Consola to remove FOUT

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,6 +7,7 @@ function About (){
         <>
             <h2 className="about__tagline"> Builder, creator, problem solver, and life long learner adventuring through the Web Development landscape. </h2>
             <p className="about__journey"><a href="#skills" className="about__journey-link">Follow me on my journey</a></p>
+            {/* <p className="about__journey"><a href="#skills" className="about__journey-link">Stick to the main trail or venture deeper through a side trail</a></p> */}
         </>
     )
 }
