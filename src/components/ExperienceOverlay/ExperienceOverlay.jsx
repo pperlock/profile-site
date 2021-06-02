9a5c4110e8282ce4ad6f3150bfa0f07c7cdee4d9
Patti Perlock <pattiperlock@gmail.com>
2021-06-02 14:32:36 -0400

    new basic layout achieved-minor adustments still needed

--- src/components/ExperienceOverlay/ExperienceOverlay.jsx
+++ src/components/ExperienceOverlay/ExperienceOverlay.jsx
@@ -6,77 +6,67 @@ import './ExperienceOverlay.scss';
  * @param {Boolean} showOverlay
  */
 
-function ExperienceOverlay({showOverlay}) {
+function ExperienceOverlay() {
 
-    // const experience=[
-    //     {id:'0', date: "2005-2010", background:"/icons/uwo.png", place: "University of Western Ontario", position: "MSc.Geophysics", focus: "programming & statistical physics", thesis: "Imaging Faults in 3D Using Pattern Informatics"},
-    //     {id:'1', date: "2000-2005", background:"/icons/uwo.png", place: "University of Western Ontario", position: "BSc.Geophysics", focus: "programming & optimization algorithms", thesis: "Modeling of the July 1998 Northridge Aftershock Using a Linear Inversion and Genetic Algorithm"},
-    //     {id:'2', date: "2017-2020", background:"/icons/klg.png", place: "Kirkland Lake Gold", position: "Senior Database Administrator"},
-    //     {id:'3', date: "2015-2017", background:"/icons/sas.png", place: "St. Andrew's Goldfields", position: "Database Administrator"},
-    //     {id:'4', date: "2010-2015", background:"/icons/dogs-north.png", place: "Dog's North", position: "Owner, Lead Trainer"},
-    //     {id:'5', date: "Present", background:"/icons/brainstation3.png", place: "BrainStation", position:"Teaching Assistant"},
-    //     {id:'6', date: "2020-2021", background:"/icons/brainstation3.png", place: "BrainStation", position: "Student: Full Stack Development"},
-    // ];
+    const events=[
+        {id:'996c7cd9', date: "Feb - May 2021", type: "professional", background:"/icons/brainstation3.png", place: "BrainStation", position:"Teaching Assistant", summary: "teaching stuff",
+            summary: "Reading and debugging code of other developers, guiding students to adhere to industry-standard best practices to produce premium styling and functionality for their web applications, teaching students to learn to solve problems using logical thinking."},
+        {id:'996c7ca8', date: "Oct 2020 - Feb 2021", type: "education", background:"/icons/brainstation3.png", place: "BrainStation", position: "Web Development Diploma", summary:"learning stuff",
+            summary: "Full-Stack Development.  Experience using provided mockups in pdf and Figma format to create respnsive applications using a mobile first approach with HTML and SASS.  Created interactive sites using both React and js DOM manipulation that pulled from APIs to serve dynamic content from outside sources.  Built Resful APIs using Node.js and express to serve up database information from MongoDB.  Dabbled in cloud storage with Firebase and passport authentication."},
+        {id:'996c7bea', date: "2017-2020", type: "professional", background:"/icons/klg.png", place: "Kirkland Lake Gold", position: "Senior Database Administrator", 
+            summary: "Spearheaded the development and execution of a SQL server database focusing on the balance between creating a user- friendly experience and data quality. Designed and regulated a detailed QAQC monitoring program that reports the precision and accuracy of assay data, improving data reliability. Collaborated with end users and software developers remotely to ensure functionality requirements were met while simultaneously providing training and support." },
+        {id:'996c7b2c', date: "2015-2017", type: "professional", background:"/icons/sas.png", place: "St. Andrew's Goldfields", position: "Database Administrator",
+            summary: "Orchestrated a validation program on an unstandardized, undocumented database to increase the accuracy of data used for making profitable business decisions."},
+        {id:'996c7a6e', date: "2010-2015", type: "professional", background:"/icons/dogs-north.png", place: "Dog's North", position: "Owner, Lead Trainer",
+            summary: "Orchestrated a validation program on an unstandardized, undocumented database to increase the accuracy of data used for making profitable business decisions."},
+        {id:'996c797e', date: "2005-2010", type: "education", background:"/icons/uwo.png", place: "University of Western Ontario", position: "MSc.Geophysics", focus: "programming & statistical physics", thesis: "Imaging Faults in 3D Using Pattern Informatics"},
+        {id:'996c7758', date: "2000-2005", type: "education", background:"/icons/uwo.png", place: "University of Western Ontario", position: "BSc.Geophysics", focus: "programming & optimization algorithms", thesis: "Modeling of the July 1998 Northridge Aftershock Using a Linear Inversion and Genetic Algorithm"}
+    ];
+
     
     return (
-        <>
-            <div className="education"> 
-                <h2 className= "education__title">EDUCATION</h2>
-                <div className="education__list">
-                    <ul className={showOverlay ? "education__list-item education__list-item--animate" : "education__list-item"}>
-                        <li className="education__list-title"> Web Development </li>
-                        <div className="education__list__place">
-                            <img className="education__list__place-image" src="/icons/brainstation3.png" alt="brainstation logo"/>
-                            <li className="education__list__place-title">Brainstation</li>
+        <div className="overlay-container">
+            <div className="timeline">
+                <div className="events">
+                    {events.map(event =>
+                        <div key={event.id} className="event">
+                            <div className = "event__date">
+                                <div className = "event__date-container">
+                                    <time className = "event__date-time">{event.date}</time>
+                                </div>
+                            </div>
+                            <div className = "event__line">
+                                <div className="event__line-marker"></div>
+                            </div>
+                            <div className = "event__description">
+                                <div className = "event__description-container">
+                                    
+                                    <div className = "event__description-header">
+                                        <div className = "event__description-details">
+                                            <p className = "event__description-position">{event.position}</p>
+                                            <p className = "event__description-place">{event.place}</p>
+                                        </div>
+                                        <div className = "event__description-icons">
+                                            <img className={event.id === "996c7b2c" ? "event__description-logo--sas" : "event__description-logo"} 
+                                                src= {event.background} 
+                                                alt="brainstation logo"/>
+                                            <img className="event__description-logo" src= {event.type ==="education" ? "icons/education-icon.png" : "icons/professional-icon.png"} alt="brainstation logo"/>
+                                        </div>
+                                    </div>
+
+                                    <div className="event__description-summary">
+                                        {event.focus && <p className = "event__description-focus"><span className="event--emphasize">Focus:</span> {event.focus} </p>}
+                                        {event.thesis && <p><span className="event--emphasize">Thesis:</span> {event.thesis} </p>}
+                                        {event.summary && <p>{event.summary}</p>}
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
-                        <li className="education__list-focus"> <span className="emphasize">Focus:</span> full-stack development</li>
-                        <li className="education__list-description"> Brainstation </li>
-                    </ul>
-                    <ul className={showOverlay ? "education__list-item education__list-item--animate2" : "education__list-item"}>
-                        <li className="education__list-title"> MSc. Geophysics</li>
-                        <div className="education__list__place">
-                            <img className="education__list__place-image" src="/icons/uwo.png" alt="university of western ontario logo"/>
-                            <li className="education__list__place-title">  University of Western Ontario</li>
-                        </div>
-                        <li className="education__list-focus"> <span className="emphasize">Focus:</span> computer programming, statistical physics</li>
-                        <li className="education__list-description"><span className="emphasize">Thesis:</span> Imaging Faults in 3D Using the Pattern
-Informatics Technique </li>
-                    </ul>
-                    <ul  className={showOverlay ? "education__list-item education__list-item--animate3" : "education__list-item"}>
-                        <li className="education__list-title"> BSc. Geophysics</li>
-                        <div className="education__list__place">
-                            <img className="education__list__place-image" src="/icons/uwo.png" alt="university of western ontario logo"/>
-                            <li className="education__list__place-title">  University of Western Ontario</li>
-                        </div>
-                        <li className="education__list-focus"> <span className="emphasize">Focus:</span> computer programming, optimization algorithms</li>
-                        <li className="education__list-description"><span className="emphasize"> Thesis:</span>  "Modeling of the July 1998 Northridge Aftershock
-Using a Linear Inversion and Genetic Algorithm" </li>
-                    </ul>
+                    )}
                 </div>
             </div>
-            
-            <div className="education"> 
-                <h2 className= "education__title">PROFESSIONAL</h2>
-                <div className="education__list">
-                    <ul className={showOverlay ? "education__list-item--professional education__list-item--animate4" : "education__list-item"}>
-                        <li className="education__list-title"> Senior Database Administrator </li>
-                        <div className="education__list__place">
-                            <img className="education__list__place-image" src="/icons/klg.png" alt="Kirkland Lake Gold logo"/>
-                            <li className="education__list__place-title">Kirkland Lake Gold</li>
-                        </div>
-                        <li className="education__list-description"> Spearheaded the development and execution of a SQL server database focusing on the balance between creating a user- friendly experience and data quality.  Designed and regulated a detailed QAQC monitoring program that reports the precision and accuracy of assay data, improving data reliability. Collaborated with end users and software developers remotely to ensure functionality requirements were met while simultaneously providing training and support. </li>
-                    </ul>
-                    <ul className={showOverlay ? "education__list-item--professional education__list-item--animate5" : "education__list-item"}>
-                        <li className="education__list-title">Database Administrator </li>
-                        <div className="education__list__place">
-                            <img className="education__list__place-image" src="/icons/sas.png" alt="St Andrews Goldfields logo"/>
-                            <li className="education__list__place-title">St. Andrews Goldfields</li>
-                        </div>
-                        <li className="education__list-description education__list-description--last"> Orchestrated a validation program on an unstandardized, undocumented database to increase the accuracy of data used for making profitable business decisions. </li>
-                    </ul>
-                </div>
-            </div>
-        </>
+        </div>
     )
 }
 
